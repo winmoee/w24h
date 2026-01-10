0b8d6ccb9a2d4a6aaf7419cb7afa13067239c380
Win Moe <frankwinmoe@gmail.com>
2026-01-10 15:56:12 -0800

    small changes

--- my-c1-app/my-c1-project/ui/src/App.tsx
+++ my-c1-app/my-c1-project/ui/src/App.tsx
@@ -4,5 +4,5 @@ import "@crayonai/react-ui/styles/index.css";
 import { C1Chat } from "@thesysai/genui-sdk";
 
 export default function App() {
-  return <C1Chat apiUrl="/api/chat" />;
+  return <C1Chat apiUrl="/api/chat" agentName="Work24Hours Chat" />;
 }
